a49d96aa240a34bb59def111c951d2bf8df6331f
Bill Kable <bkable@gmail.com>
2020-02-05 00:41:11 -0600

    Add task for migrating databases

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -4,11 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import com.mysql.cj.jdbc.MysqlDataSource;
+
 @SpringBootApplication
 public class PalTrackerApplication {
 	@Bean
 	public TimeEntryRepository getTimeEntryRepository() {
-		return new InMemoryTimeEntryRepository();
+		MysqlDataSource dataSource = new MysqlDataSource();
+        dataSource.setUrl(System.getenv("SPRING_DATASOURCE_URL"));
+		return new JdbcTimeEntryRepository(dataSource);
 	}
 	public static void main(String[] args) {
         SpringApplication.run(PalTrackerApplication.class, args);

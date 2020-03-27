3dc75e649705c4e05796c0b542ef52b0ba226daf
Praveen-Kumar-cts <Praveen.Kumar208a0d@cognizant.com>
2020-03-27 18:40:38 +0530

    Db connection

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -8,10 +8,10 @@ import com.mysql.cj.jdbc.MysqlDataSource;
 
 @SpringBootApplication
 public class PalTrackerApplication {
+	
 	@Bean
 	public TimeEntryRepository getTimeEntryRepository() {
 		MysqlDataSource dataSource = new MysqlDataSource();
-        dataSource.setUrl(System.getenv("SPRING_DATASOURCE_URL"));
 		return new JdbcTimeEntryRepository(dataSource);
 	}
 	public static void main(String[] args) {
